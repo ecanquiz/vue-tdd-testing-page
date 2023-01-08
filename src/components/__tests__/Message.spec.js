354b759da61858d01a49258708abf11567a984d2
Ernesto Canquiz <fdsoil123@gmail.com>
2023-01-08 18:50:17 -0500

    feat: My first commit && Add a validator

---
+++ src/components/__tests__/Message.spec.js
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/vue'
+import Message,{ validateVariant } from '../Message.vue'
+
+describe('Message', () => {
+  it('renders variant correctly when passed', () => {
+    const { container } = render(Message, {
+      props: {
+        variant: 'success'
+      }
+    })
+  
+    expect(container.firstChild.classList).toContain(['success'])
+  })
+
+  it('validates valid variant prop', () => {
+    ['success', 'warning', 'error'].forEach(variant => {
+      expect(() => validateVariant(variant)).not.toThrow()
+    })
+  })
+  
+  it('throws error for invalid variant prop', () => {
+    expect(() => validateVariant('invalid')).toThrow()
+  })
+})
