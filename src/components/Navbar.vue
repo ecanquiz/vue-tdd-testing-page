316368bb69abcf49913dd295a4d4a049aabeefd3
Ernesto Canquiz <fdsoil123@gmail.com>
2023-01-10 12:57:24 -0500

    feat: Separation of concerns: key concept and case study

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <button v-if="authenticated">Logout</button>
+  <button v-if="!authenticated">Login</button>
+</template>
+
+<script>
+export default {
+  props: {
+    authenticated: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
