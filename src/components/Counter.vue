e11e411f54301d4ea7eee41f24f4034d316b97f2
Ernesto Canquiz <fdsoil123@gmail.com>
2023-01-11 15:36:44 -0500

    feat: Finishing chapter 4

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,14 +1,34 @@
-<template>
-  <button role="increment" @click="count += 1" />
-  <button role="submit" @click="$emit('submit', count)" />
-</template>
-
 <script>
-export default {
-  data() {
-    return {
-      count: 0
-    }
+export function submitValidator(count) {
+  if (typeof count === 'string' || isNaN (count)) {
+    throw Error(`
+      Count should be a number.
+      Got: ${count}
+    `)
   }
+  return true
 }
 </script>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const emit = defineEmits({
+  submit: submitValidator
+})
+
+const increment = () => {
+  count.value += 1
+}
+const submit = () => {
+  emit('submit', count.value)
+}
+</script>
+
+<template>
+  <button role="increment" @click="increment" />
+  <button role="submit" @click="submit" />
+</template>
+
