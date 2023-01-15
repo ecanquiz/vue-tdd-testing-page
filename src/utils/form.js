74facbad111a8b73edd034f6ec7d60c0428bd6f4
Ernesto Canquiz <fdsoil123@gmail.com>
2023-01-14 19:37:39 -0500

    feat: The Form Object and Full Form Validation

--- src/utils/form.js
+++ src/utils/form.js
@@ -8,6 +8,29 @@ export function isBetween(value, { min, max }) {
   return { valid: true }
 }
 
+export function isFormValid(form) {
+  return form.name.valid && form.weight.valid
+}
+
+const limits = {
+  kg: { min: 30, max: 200 },
+  lb: { min: 66, max: 440 },
+}
+
+export function patientForm(patient) {
+  const name = required(patient.name)
+
+  const weight = validateMeasurement(patient.weight.value, {
+    nullable: false,
+    constraints: limits[patient.weight.units]
+  })
+
+  return {
+    name,
+    weight
+  }
+}
+
 export function required(value) {
   if (!value) {
     return {
